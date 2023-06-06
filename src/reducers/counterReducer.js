42fbaae522aeaf786b94e444d7b8cf4182c1a675
kamruddin Ansari <114280257+Kamruddin1@users.noreply.github.com>
2023-06-06 11:43:21 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,4 +1,18 @@
+
 const counterReducer = (state,action) =>{
+
+    switch(action.type){
+        case "INCREMENT":
+            return {count : state.count +1};
+        case "DECREMENT":
+             return  {count: state.count-1}
+                
+        default :
+           return state;
+
+    }
+
+
 
 }
 
